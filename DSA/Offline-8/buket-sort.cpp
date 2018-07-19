545e029fdeb22f30be9697966af886ac5269f17c
rebornplusplus <rebornplusplus@gmail.com>
2018-07-19 15:20:21 +0600

    Create buket-sort.cpp

---
+++ DSA/Offline-8/buket-sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double X2_MAX = 100000;	// X2_MAX = 2 * max(abs(a[i])); double the maximum absolute val
+const int NOB = 5;	// Number of Buckets
+vector< double > buket[NOB];	// The Buckets
+
+void buket_sort(double *a, int n) {
+	for(int i=0; i<NOB; ++i) buket[i].clear();
+	for(int i=0; i<n; ++i) {
+		a[i] = (a[i]/X2_MAX) + 0.5;
+		int which = (a[i] * NOB);
+		buket[which].push_back(a[i]);
+	}
+	for(int i=0; i<NOB; ++i) sort(buket[i].begin(), buket[i].end());
+	for(int i=0, j=0; i<NOB; ++i) {
+		for(int k=0; k<(int) buket[i].size(); ++k) {
+			a[j++] = (buket[i][k] - 0.5) * X2_MAX;
+		}
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	double a[n];
+	for(int i=0; i<n; ++i) cin >> a[i];
+
+	buket_sort(a, n);
+	for(int i=0; i<n; ++i) cout << a[i] << " ";
+	cout << "\n";
+
+	return 0;
+}
